d69e5cec7f321e630c33d03bc6528eeef5db88b3
jose.racero <jose.racero@piksel.com>
2021-11-05 15:56:28 +0100

    new endpoint to process the charging

--- pleo-antaeus-rest/src/main/kotlin/io/pleo/antaeus/rest/AntaeusRest.kt
+++ pleo-antaeus-rest/src/main/kotlin/io/pleo/antaeus/rest/AntaeusRest.kt
@@ -8,6 +8,7 @@ import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.apibuilder.ApiBuilder.path
 import io.pleo.antaeus.core.exceptions.EntityNotFoundException
+import io.pleo.antaeus.core.services.BillingService
 import io.pleo.antaeus.core.services.CustomerService
 import io.pleo.antaeus.core.services.InvoiceService
 import mu.KotlinLogging
@@ -17,7 +18,8 @@ private val thisFile: () -> Unit = {}
 
 class AntaeusRest(
     private val invoiceService: InvoiceService,
-    private val customerService: CustomerService
+    private val customerService: CustomerService,
+    private val billingService: BillingService
 ) : Runnable {
 
     override fun run() {
@@ -61,6 +63,10 @@ class AntaeusRest(
                             it.json(invoiceService.fetchAll())
                         }
 
+                        get(("pending")) {
+                            it.json(invoiceService.fetchPendingInvoices())
+                        }
+
                         // URL: /rest/v1/invoices/{:id}
                         get(":id") {
                             it.json(invoiceService.fetch(it.pathParam("id").toInt()))
@@ -76,6 +82,12 @@ class AntaeusRest(
                         // URL: /rest/v1/customers/{:id}
                         get(":id") {
                             it.json(customerService.fetch(it.pathParam("id").toInt()))
+                        }
+                    }
+                    path("charge") {
+                        // URL: /rest/v1/charge
+                        get {
+                            it.json(billingService.applyCharges().toString())
                         }
                     }
                 }
