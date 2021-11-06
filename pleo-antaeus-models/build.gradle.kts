e17d11d5deb85978c27d12ba89b27bfc46b55b04
jose.racero <jose.racero@piksel.com>
2021-11-06 18:08:37 +0100

    required library for dates

--- pleo-antaeus-models/build.gradle.kts
+++ pleo-antaeus-models/build.gradle.kts
@@ -7,4 +7,5 @@ kotlinProject()
 dependencies {
     implementation("com.google.code.gson:gson:2.8.5");
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.8.+")
+    implementation("joda-time:joda-time:2.10.2")
 }
