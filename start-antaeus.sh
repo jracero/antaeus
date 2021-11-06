d8409e93db4c24a895343a4075b0efd93e53d1f8
jose.racero <jose.racero@piksel.com>
2021-11-06 09:59:49 +0100

    new scheduler implementation

---
+++ start-antaeus.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# stop containers if they are already running
+docker stop antaeus_app
+docker rm antaeus_app
+docker stop antaeus_scheduler
+docker rm antaeus_scheduler
+
+#Starting containers
+echo "starting antaeus app..."
+docker run -d --name antaeus_app -p 7000:7000 antaeus
+sleep 120
+echo "starting antaeus scheduler..."
+docker run --name antaeus_scheduler --network=host antaeus-scheduler
